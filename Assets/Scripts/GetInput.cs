e537ee621b86ff8d83a1d7bc172836559e0b743f
Piotr Masior <fokstrot76@gmail.com>
2015-10-11 17:36:28 +0100

    Mathematical Challenge from scratch
    I built the new game with main menu level and just initiated level 1

---
+++ Assets/Scripts/GetInput.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GetInput : MonoBehaviour {
+
+    private InputField input;
+    private string nickname;
+	// Use this for initialization
+	void Start () {
+        input = GetComponent<InputField>();
+	}
+	
+    //settter and getter
+	public void setNickName (InputField name) {
+        nickname = name.text;
+	}
+    public string getNickName()
+    {
+        return nickname;
+    }
+}
---
+++ Assets/Scripts/menuScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;// we need this namespace in order to access UI elements within our script
+using UnityEngine.Events;
+
+public class menuScript : MonoBehaviour
+{
+ 
+    public Button startGame;
+    public Button exitGame;
+    public InputField input;
+    public GetInput name;
+  
+
+    public void Start()
+    {
+        input = GetComponent<InputField>();
+    }
+
+    public void MENU_ACTION_GoToPage(string sceneName)
+    {
+
+        if (string.IsNullOrEmpty(name.getNickName()))
+        { 
+            print("You haven't type your nickname");
+        }
+        else
+        {
+            Application.LoadLevel(sceneName);
+            print("NickName: " + name.getNickName());
+        }
+        
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        print("Game Exit");
+    }
+
+}
